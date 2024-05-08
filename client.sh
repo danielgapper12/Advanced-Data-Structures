2bbe96d6ecc2ec8bf7b5dbb848a2a27dfd1756bd
danielgapper12 <156476802+danielgapper12@users.noreply.github.com>
2024-05-08 12:47:39 -0700

    Add files via upload

---
+++ client.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# @author: Ankit Dimri
+# Shell script to run the client which connects to server MISTCP
+
+# Compile client code and run
+echo
+echo "Compiling client.cpp..."
+# compiling
+g++ -std=c++14 client.cpp -o client
+echo
+echo
+echo "client.cpp compiled..."
+echo
+echo "Starting client..."
+echo
+
+count=0
+ps_running=$(pidof client)
+for pids in $ps_running
+do
+  count=`expr $count + 1`
+done
+
+count=`expr $count + 1`
+
+echo "Starting client $count ..."
+
+#run the object file
+./client
